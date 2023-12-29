c53e0f151250b8ce5d11020de59fb570dc77065f
Bartek Boćkowski <bboc12357@gmail.com>
2023-12-29 18:08:04 +0100

    optimization

--- src/main/java/com/example/carparkinggateway/config/GatewayConfig.java
+++ src/main/java/com/example/carparkinggateway/config/GatewayConfig.java
@@ -1,19 +1,10 @@
 package com.example.carparkinggateway.config;
 
-import com.example.carparkinggateway.exception.AccessDeniedException;
 import com.example.carparkinggateway.service.BannedIPService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,35 +12,9 @@ public class GatewayConfig {
 
     private final BannedIPService bannedIPService;
 
-    @Value("${apiKey}")
-    private String apiKey;
-
     @Bean
     public GlobalFilter customFilter() {
-        return new CustomGlobalFilter();
-    }
-
-    public class CustomGlobalFilter implements GlobalFilter, Ordered {
-
-        @Override
-        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-            ServerHttpRequest request = exchange.getRequest();
-
-            String ip = request.getRemoteAddress().getAddress().toString();
-            if (bannedIPService.existsByIp(ip)) {
-                throw new AccessDeniedException("Access denied - your IP is banned.");
-            }
-
-            if (Objects.isNull(request.getHeaders().get("apiKey")) || !Objects.equals(request.getHeaders().get("apiKey").get(0), apiKey)) {
-                throw new AccessDeniedException("Access denied - wrong API key.");
-            }
-            return chain.filter(exchange);
-        }
-
-        @Override
-        public int getOrder() {
-            return -1;
-        }
+        return new CustomGlobalFilter(bannedIPService);
     }
 
 }
--- src/main/java/com/example/carparkinggateway/exception/AccessDeniedException.java
+++ src/main/java/com/example/carparkinggateway/exception/AccessDeniedException.java
@@ -1,5 +1,8 @@
 package com.example.carparkinggateway.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
 public class AccessDeniedException extends RuntimeException {
 
     public AccessDeniedException(String message) {
